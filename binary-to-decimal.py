e668fb600477abef315f54201c484cd0a5d7d0a8
noob-dude27 <80330450+noob-dude27@users.noreply.github.com>
2023-10-27 00:41:05 +0800

    Update binary-to-decimal.py
    improved code formatting, attempted to apply PEP8 guidelines

--- binary-to-decimal.py
+++ binary-to-decimal.py
@@ -1,7 +1,8 @@
-# Program intended to convert binary to decimal numbers
-# Program was intended to assist in answering some CSS questions inwhich contained binary-to-decimal conversions.
+# Program intended to convert binary to decimal numbers.
+# Program was intended to assist in answering some CSS questions inwhich
+# contained binary-to-decimal conversions.
 
-def verify_binary(binary:int) -> bool:
+def verify_binary(binary: int) -> bool:
     """Verifies that the given binary digits are actually convertible to decimal."""
     # First check: Checks if all are digits by converting the binary into int
     try:
@@ -16,38 +17,50 @@ def verify_binary(binary:int) -> bool:
         if digit not in digit_list:
             print("Please enter 1s and 0s only! No letters!")
             return False
-    return True        
-    
-def init_num_list(interval:int) -> list:
-    """Creates "num_list", the numbers inside the list will be used to align with each binary digit."""
+    return True
+
+
+def init_num_list(interval: int) -> list:
+    """
+    Creates "num_list". The numbers inside the list will be used
+    to align with each binary digit.
+    """
     num_list = []
 
-    num = 1 # CONSTANT
-    starting_num = num # the original number is added before increment since it is part of the alignment. 
+    num = 1  # CONSTANT
+    # Adds original number before increment since it is part of the alignment.
+    starting_num = num
     num_list.append(starting_num)
 
-    # Subtracts old interval by 1 then appends the original number to the first index for a correct "num_list".
+    # Subtracts old interval by 1 then appends the original number
+    # to the first index for a correct "num_list".
     new_interval = interval-1
     i = 1
     while i <= new_interval:
-        # summation occurs until "i" exceeds "new_interval"
-        # resulting summation values are appended to the "num_list".
+        # Summation occurs until "i" exceeds "new_interval"
+        # Appends resulting summation values to the "num_list".
         i += 1
         num += num
         num_list.append(num)
     return num_list
 
-def align_to_decimal(binary:str, num_list:list) -> list:
-    """Matches the binary digits with their correspoding values."""
+
+def align_to_decimal(binary: str, num_list: list) -> list:
+    """Matches the binary digits with their correspoding values in num_list."""
     alignment = []
 
     for value, digit in zip(num_list, reversed(binary)):
         tmp = [value, digit]
         alignment.append(tmp)
+    print(f"Before removing zeroes: {alignment}")
     return alignment
 
+
 def filter_digit_alignment(alignment):
-    """Removes the unnecessary alignments then creates the final version of "alignment" list only containing decimals."""
+    """
+    Removes the unnecessary alignments then creates the final version of
+    "alignment" list only containing decimals.
+    """
     # Removes the digit and its corresponding decimal if the digit is 0.
     for row in alignment[:]:
         if row[1] == "0":
@@ -58,35 +71,43 @@ def filter_digit_alignment(alignment):
     for row in alignment[:]:
         row.remove('1')
 
-    # Finally, extract the remaining values from "alignment" and insert to "final_alignment" to add all numbers.
+    # Finally, extracts the remaining values from "alignment"
+    # and inserts to "final_alignment" to add all numbers.
     final_alignment = []
     for row in alignment:
         for items in row:
-            values = items
-            final_alignment.append(values)
-    
+            final_alignment.append(items)
+
     print(f"Values to be summed: {final_alignment}")
     return final_alignment
 
+
 def combine_values(final_alignment):
-    """The answer is obtained after summing the contents of the entire list."""
+    """
+    The answer is finally obtained after the program sums the
+    contents of the entire list.
+    """
     answer = sum(final_alignment)
     return answer
 
-if __name__ == "__main__":
-    # Before immediately proceeding into converting the binary, a verification process takes place.
-    # The verification checks for non-binary digits within "binary".
-    binary = input("Enter a set of binary digits here: ")
-    is_binary = verify_binary(binary)
 
-    if is_binary:
-        binary_length = len(binary)
-        
-        num_list = init_num_list(binary_length)
-        alignment = align_to_decimal(binary, num_list)
-        final_alignment = filter_digit_alignment(alignment)
-        answer = combine_values(final_alignment)
-        
-        print(f"Binary to Decimal: {binary} => {answer}\n")
-    else:
-        print("Conversion was not successful.\n")
+if __name__ == "__main__":
+    # Before immediately proceeding into converting the binary,
+    # a verification process takes place.
+    #
+    # The verification checks for non-binary digits within  the given binary.
+    while True:
+        binary = input("Enter a set of binary digits here: ")
+        is_binary = verify_binary(binary)
+
+        if is_binary:
+            binary_length = len(binary)
+
+            num_list = init_num_list(binary_length)
+            alignment = align_to_decimal(binary, num_list)
+            final_alignment = filter_digit_alignment(alignment)
+            answer = combine_values(final_alignment)
+
+            print(f"Binary to Decimal: {binary} => {answer}\n")
+        else:
+            print("Conversion was not successful.\n")
